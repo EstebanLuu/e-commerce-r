1e646020c8ae041e3b8b8010be97691da5d08f79
EstebanLuu <estebaanlunaaa@gmail.com>
2022-08-11 23:58:15 -0300

    2.0

--- src/components/wrapper/Wrapper.jsx
+++ src/components/wrapper/Wrapper.jsx
@@ -1,48 +1,48 @@
-import React from "react"
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 const Wrapper = () => {
   const data = [
     {
-      cover: <i class='fa-solid fa-truck-fast'></i>,
-      title: "Worldwide Delivery",
-      decs: "We offer competitive prices on our 100 million plus product any range.",
+      cover: <i class="fa-solid fa-truck-fast"></i>,
+      title: "Envio a todo el mundo",
+      decs: "Ofrecemos envios economicos a todas partes del mundo",
     },
     {
-      cover: <i class='fa-solid fa-id-card'></i>,
-      title: "Safe Payment",
-      decs: "We offer competitive prices on our 100 million plus product any range.",
+      cover: <i class="fa-solid fa-id-card"></i>,
+      title: "Pago seguro",
+      decs: "Ofrecemos seguridad a la hora de pagar nuestros productos",
     },
     {
-      cover: <i class='fa-solid fa-shield'></i>,
-      title: "Shop With Confidence ",
-      decs: "We offer competitive prices on our 100 million plus product any range.",
+      cover: <i class="fa-solid fa-shield"></i>,
+      title: "Compra con confianza",
+      decs: "Ofrecemos garantia para que el consumidor este 100% satisfecho con nuestro servicio",
     },
     {
-      cover: <i class='fa-solid fa-headset'></i>,
-      title: "24/7 Support ",
-      decs: "We offer competitive prices on our 100 million plus product any range.",
+      cover: <i class="fa-solid fa-headset"></i>,
+      title: "Soporte 24/7",
+      decs: "Ofrecemos precios competitivos en nuestros más de 100 millones de productos de cualquier rango.",
     },
-  ]
+  ];
   return (
     <>
-      <section className='wrapper background'>
-        <div className='container grid2'>
+      <section className="wrapper background">
+        <div className="container grid2">
           {data.map((val, index) => {
             return (
-              <div className='product' key={index}>
-                <div className='img icon-circle'>
+              <div className="product" key={index}>
+                <div className="img icon-circle">
                   <i>{val.cover}</i>
                 </div>
                 <h3>{val.title}</h3>
                 <p>{val.decs}</p>
               </div>
-            )
+            );
           })}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Wrapper
+export default Wrapper;
